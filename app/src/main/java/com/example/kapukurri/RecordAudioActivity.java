0a0efa730ecb8ced7a068afc57c898bc1149ba18
Paul Wade <paul6325106@gmail.com>
2015-01-29 23:41:40 +1000

    Probably records/plays audio files
    Audio/external storage stuff:
    
    http://www.techotopia.com/index.php/Android_Audio_Recording_and_Playback_using_MediaPlayer_and_MediaRecorder
    
    Error handling/async:
    
    http://www.101apps.co.za/articles/recording-and-playing-back-audio-files.html

--- app/src/main/java/com/example/kapukurri/RecordAudioActivity.java
+++ app/src/main/java/com/example/kapukurri/RecordAudioActivity.java
@@ -1,21 +1,62 @@
 package com.example.kapukurri;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
+import android.media.MediaRecorder;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class RecordAudioActivity extends ActionBarActivity {
+
+    private static MediaRecorder mediaRecorder;
+
+    private static String audioFilePath;
+//    private static Button stopButton;
+//    private static Button recordButton;
+
+    private boolean isRecording = false;
+
+    protected boolean hasMicrophone() {
+        PackageManager pmanager = this.getPackageManager();
+        return pmanager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//        recordButton = (Button) findViewById(R.id.recordButton);
+//        stopButton = (Button) findViewById(R.id.stopButton);
+
+//        if (!hasMicrophone()) {
+//            stopButton.setEnabled(false);
+//            recordButton.setEnabled(false);
+//        } else {
+//            stopButton.setEnabled(false);
+//        }
+    }
+
+    protected static String getAudioFilepath() {
+        return Environment.getExternalStorageDirectory().getAbsolutePath() + "/"
+                + (new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss").format(new Date()))
+                + ".3gp";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_audio);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,6 +87,42 @@ public class RecordAudioActivity extends ActionBarActivity {
         if(newConfig.orientation == Configuration.ORIENTATION_PORTRAIT)
         {
             startActivity(new Intent(this, FamilyTreeActivity.class));
+        }
+    }
+
+    private void startRecording () {
+        if (isRecording) stopRecording();
+
+        isRecording = true;
+//        stopButton.setEnabled(true);
+//        recordButton.setEnabled(false);
+
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            String filepath = getAudioFilepath();
+            mediaRecorder.setOutputFile(filepath);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+            mediaRecorder.start();
+            //TODO write filepath to database
+        } catch (IOException e) {
+            e.printStackTrace();
+            //TODO indicate malfunction to user
+        }
+    }
+
+    private void stopRecording() {
+//        stopButton.setEnabled(false);
+
+        if (isRecording) {
+            mediaRecorder.stop();
+            //mediaRecorder.reset();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            isRecording = false;
+//            recordButton.setEnabled(true);
         }
     }
 }
--- app/src/main/java/com/example/kapukurri/StoriesActivity.java
+++ app/src/main/java/com/example/kapukurri/StoriesActivity.java
@@ -2,20 +2,42 @@ package com.example.kapukurri;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+
 
 public class StoriesActivity extends ActionBarActivity {
+
+    private static MediaPlayer mediaPlayer;
+
+    private static String audioFilePath;
+//    private static Button stopButton;
+//    private static Button playButton;
+
+    private boolean isPlaying = false;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+//        playButton = (Button) findViewById(R.id.playButton);
+//        stopButton = (Button) findViewById(R.id.stopButton);
+
+//        playButton.setEnabled(true);
+//        stopButton.setEnabled(false);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stories);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -46,6 +68,32 @@ public class StoriesActivity extends ActionBarActivity {
         if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE)
         {
             startActivity(new Intent(this, RecordAudioActivity.class));
+        }
+    }
+
+    private void startPlaying() throws IOException {
+//        playButton.setEnabled(false);
+//        stopButton.setEnabled(true);
+
+        if (isPlaying) stopPlaying();
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setDataSource(audioFilePath);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+        isPlaying = true;
+    }
+
+    private void stopPlaying() {
+//        stopButton.setEnabled(false);
+//        playButton.setEnabled(true);
+
+        if (isPlaying) {
+            mediaPlayer.stop();
+            //mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+            isPlaying = false;
         }
     }
 }
